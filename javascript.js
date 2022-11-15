05aac95b8d8cf456698eb422b0f7c8db2944c85e
Alex Hoffman <111666894+TellemBigAlsentya@users.noreply.github.com>
2022-11-15 12:22:14 -0800

    Tic Tac Toe 4x4

--- javascript.js
+++ javascript.js
@@ -8,14 +8,16 @@ const playerTwo = 'Player Two';
 let currentPlayer = playerOne;
 const playerPhrase = document.getElementById('playerPhrase');
 const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15], 
+    [0, 5, 10, 15],
+    [3, 6, 9, 12]
 ];
 let boardState = Array(cells.length).fill('');
 var gameOver = false;
@@ -65,8 +67,9 @@ function checkForWin(clickedCell) {
         let oneInARow = boardState[winCombo[0]];
         let twoInARow = boardState[winCombo[1]];
         let threeInARow = boardState[winCombo[2]];
+        let fourInARow = boardState[winCombo[3]];
         
-        if(oneInARow !== '' && oneInARow === twoInARow && oneInARow === threeInARow) {
+        if(oneInARow !== '' && oneInARow === twoInARow && oneInARow === threeInARow && oneInARow === fourInARow) {
             gameOver = true; 
             endGameMessage.innerHTML = `${oneInARow} wins!`; 
         }
